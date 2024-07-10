733762efc3be0b8dd31f7cf5dcbb564a82e00408
Coder-Pirate <mdminhajulislam.contact@gmail.com>
2024-07-10 19:52:54 +0600

    remove unuse code

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,19 +1,4 @@
-{{-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-        @livewireStyles
-    </head>
-    <body>
-        {{ $slot }}
-        Hello   
-
-        @livewireScripts
-    </body>
-</html> --}}
 
 
 
@@ -79,55 +64,6 @@
 <!-- /navigation -->
 
 {{ $slot }}
-{{-- <div class="modal applyLoanModal fade" id="applyLoan" tabindex="-1" aria-labelledby="applyLoanLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header border-bottom-0">
-        <h4 class="modal-title" id="exampleModalLabel">How much do you need?</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form action="#!" method="post">
-          <div class="row">
-            <div class="col-lg-6 mb-4 pb-2">
-              <div class="form-group">
-                <label for="loan_amount" class="form-label">Amount</label>
-                <input type="number" class="form-control shadow-none" id="loan_amount" placeholder="ex: 25000">
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4 pb-2">
-              <div class="form-group">
-                <label for="loan_how_long_for" class="form-label">How long for?</label>
-                <input type="number" class="form-control shadow-none" id="loan_how_long_for" placeholder="ex: 12">
-              </div>
-            </div>
-            <div class="col-lg-12 mb-4 pb-2">
-              <div class="form-group">
-                <label for="loan_repayment" class="form-label">Repayment</label>
-                <input type="number" class="form-control shadow-none" id="loan_repayment" disabled>
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4 pb-2">
-              <div class="form-group">
-                <label for="loan_full_name" class="form-label">Full Name</label>
-                <input type="text" class="form-control shadow-none" id="loan_full_name">
-              </div>
-            </div>
-            <div class="col-lg-6 mb-4 pb-2">
-              <div class="form-group">
-                <label for="loan_email_address" class="form-label">Email address</label>
-                <input type="email" class="form-control shadow-none" id="loan_email_address">
-              </div>
-            </div>
-            <div class="col-lg-12">
-              <button type="submit" class="btn btn-primary w-100">Get Your Loan Now</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div> --}}
 
 
 
@@ -147,16 +83,7 @@
 						</li>
 						@endforeach
 
-						{{-- <li class="mb-2"><a href="service-details.html">Digital Marketing</a>
-						</li> --}}
-						{{-- <li class="mb-2"><a href="service-details.html">Web Design</a>
-						</li>
-						<li class="mb-2"><a href="service-details.html">Logo Design</a>
-						</li>
-						<li class="mb-2"><a href="service-details.html">Graphic Design</a>
-						</li>
-						<li class="mb-2"><a href="service-details.html">SEO</a>
-						</li> --}}
+						
 					</ul>
 				</div>
 			</div>
